80001b2cd2b4f0ac2190039cf1cc7c13cef4a203
Ariel-ttw <61923883+Ariel-ttw@users.noreply.github.com>
2021-10-12 21:40:58 +1000

    Add files via upload

---
+++ src/pop.php
@@ -0,0 +1,30 @@
+<?php 
+include_once 'header.php';
+?>
+
+<!DOCTYPE php>
+<php lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>success</title>
+    <link rel="stylesheet" type="text/css" href="css/pop.css">
+</head>
+
+<body>
+<h1>Confirm your booking require</h1>
+<div class="box">
+	<a class="button" href="#popup1">confirm booking</a>
+</div>
+
+<div id="popup1" class="overlay">
+	<div class="popup">
+		<h2>success</h2>
+		<a class="close" href="#">&times;</a>
+		<div class="content">
+			Thank to for your booking, your booking information will send by email.
+		</div>
+	</div>
+</div>
+</body>
